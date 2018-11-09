0273c1f58ffdfbe50e0092f6a5cce6e73ab61b7e
JIAHUI J <jiahui.jiang@biola.edu>
2018-11-09 13:05:02 -0800

    Main Menu Desgin, remove action bar, navigation bar, rounded button , button shadow xml.

--- app/src/main/java/com/example/derekjames/uiproject/Main_Menu.java
+++ app/src/main/java/com/example/derekjames/uiproject/Main_Menu.java
@@ -2,12 +2,19 @@ package com.example.derekjames.uiproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Main_Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main__menu);
+
+
     }
 }

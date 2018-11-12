2812171494b634b4aa76448eca50eb57866b4ca7
JIAHUI J <jiahui.jiang@biola.edu>
2018-11-12 14:07:55 -0800

    Change chime activitie to tab activitie.

--- app/src/main/java/com/example/derekjames/uiproject/Main_Menu.java
+++ app/src/main/java/com/example/derekjames/uiproject/Main_Menu.java
@@ -1,10 +1,12 @@
 package com.example.derekjames.uiproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Main_Menu extends AppCompatActivity {
 
@@ -15,6 +17,52 @@ public class Main_Menu extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main__menu);
 
+        Button ChimeBtn = findViewById(R.id.ChimeNewsBtn);
+        Button BuildingHourBtn = findViewById(R.id.BuildingHourBtn);
+        Button CampusMapBtn = findViewById(R.id.CampusMapBtn);
+        Button EateiresBrn = findViewById(R.id.CampusFoodBtn);
+
+        ChimeBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main_Menu.this,
+                        com.example.derekjames.uiproject.Chime_Main_Tab.class);
+                startActivity(intent);
+            }
+        });
+
+        BuildingHourBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main_Menu.this,
+                        com.example.derekjames.uiproject.Chime_Main_Tab.class);
+                startActivity(intent);
+            }
+        });
+
+        CampusMapBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main_Menu.this,
+                        com.example.derekjames.uiproject.Chime_Main_Tab.class);
+                startActivity(intent);
+            }
+        });
+
+        EateiresBrn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main_Menu.this,
+                        com.example.derekjames.uiproject.Chime_Main_Tab.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
     }
 }

be8bf98d81dbd0136924b3fe6560343c87534878
DerekJGit <derekjames@q.com>
2018-11-12 15:56:25 -0800

    Restaurant ListView Added

--- app/src/main/java/com/example/derekjames/uiproject/Restaurants_Main.java
+++ app/src/main/java/com/example/derekjames/uiproject/Restaurants_Main.java
@@ -1,7 +1,16 @@
 package com.example.derekjames.uiproject;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Restaurants_Main extends AppCompatActivity {
 
@@ -9,5 +18,32 @@ public class Restaurants_Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants__main);
+
+
+        String[] restaurantsArray = new String[] {
+                "Eagles Nest",
+                "Common Grounds",
+                "Heritage Cafe",
+                "Talon",
+                "Caf",
+                "Coffee Cart"
+        };
+        final ListView restaurantListView = (ListView) findViewById(R.id.RestaurantsListView);
+        final List<String> myList = new ArrayList<String>(Arrays.asList(restaurantsArray));
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, myList) {
+            @Override public View getView(int pos, View convertView, ViewGroup parent) {
+                View view = super.getView(pos, convertView,parent);
+                ViewGroup.LayoutParams parameters = view.getLayoutParams();
+                parameters.height = 400;
+                view.setLayoutParams(parameters);
+                return  view;
+            }
+        };
+        restaurantListView.setAdapter(arrayAdapter);
+
+
+
+
+
     }
 }

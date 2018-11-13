8fe2597ed60009fd4d2f321d5af7f630005ba6da
DerekJGit <derekjames@q.com>
2018-11-12 22:06:32 -0800

    building page created

--- app/src/main/java/com/example/derekjames/uiproject/Restaurants_Food_Menus.java
+++ app/src/main/java/com/example/derekjames/uiproject/Restaurants_Food_Menus.java
@@ -62,16 +62,15 @@ public class Restaurants_Food_Menus extends AppCompatActivity {
 
 
         ////////////////adds array into the listview
-        String[] restaurantsArray = new String[] {
-                "Menu Item 1: jskldfjskldajfksdjfkjsdkfjdksjfskeoerjdjxknvsdfakljdfkljeiojs",
-                "Menu Item 2: jskldfjskldajfksdjfkjsdkfjdksjfskeoerjdjxknvsdfakljdfkljeiojs",
-                "Menu Item 3: jskldfjskldajfksdjfkjsdkfjdksjfskeoerjdjxknvsdfakljdfkljeiojs",
-                "Menu Item 4: jskldfjskldajfksdjfkjsdkfjdksjfskeoerjdjxknvsdfakljdfkljeiojs",
-                "Menu Item 5: jskldfjskldajfksdjfkjsdkfjdksjfskeoerjdjxknvsdfakljdfkljeiojs",
-                "Menu Item 6: jskldfjskldajfksdjfkjsdkfjdksjfskeoerjdjxknvsdfakljdfkljeiojs",
-                "Menu Item 7: jskldfjskldajfksdjfkjsdkfjdksjfskeoerjdjxknvsdfakljdfkljeiojs",
+       String[] restaurantsArray = new String[] {
+                "Menu Item 1: jskldfjskldajkiojs",
+                "Menu Item 3: jskldfjskldajfksds",
+                "Menu Item 4: jskldfjskldajfeiojs",
+                "Menu Item 5: jskldfjskiojs",
+                "Menu Item 6: jskldfjskojs",
+                "Menu Item 7: jskldfjskldajfksiojs",
         };
-        final ListView restaurantListView = (ListView) findViewById(R.id.RestaurantsListView);
+        final ListView restaurantListView = (ListView) findViewById(R.id.RestaurantsMenuListView);
         final List<String> myList = new ArrayList<String>(Arrays.asList(restaurantsArray));
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, myList) {
             @Override public View getView(int pos, View convertView, ViewGroup parent) {

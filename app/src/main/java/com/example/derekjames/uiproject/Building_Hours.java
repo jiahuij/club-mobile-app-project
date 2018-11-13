ece08fb2df1ec181d49f75627d9c9f48564a805d
DerekJGit <derekjames@q.com>
2018-11-12 16:47:54 -0800

    Restaurant ListView Added

--- app/src/main/java/com/example/derekjames/uiproject/Building_Hours.java
+++ app/src/main/java/com/example/derekjames/uiproject/Building_Hours.java
@@ -1,7 +1,11 @@
 package com.example.derekjames.uiproject;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Building_Hours extends AppCompatActivity {
 
@@ -9,5 +13,42 @@ public class Building_Hours extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building__hours);
+
+        /////Switch between tabs
+        BottomNavigationView tabBar = (BottomNavigationView) findViewById(R.id.tabBar);
+        tabBar.setSelectedItemId(R.id.buildinghoursicon);
+        tabBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.chimesicon : ///////Go to chimes page
+                        Intent intent1 = new Intent(Building_Hours.this,
+                                com.example.derekjames.uiproject.Chime_Main_Tab.class);
+                        startActivity(intent1);
+
+                        return true;
+                    case R.id.mapicon :
+                        Intent intent2 = new Intent(Building_Hours.this,
+                                com.example.derekjames.uiproject.Map.class);
+                        startActivity(intent2);
+                        return true;
+                    case R.id.buildinghoursicon :
+                        Intent intent3 = new Intent(Building_Hours.this,
+                                com.example.derekjames.uiproject.Building_Hours.class);
+                        startActivity(intent3);
+                        return true;
+                    case R.id.restaurantsicon :
+                        Intent intent4 = new Intent(Building_Hours.this,
+                                com.example.derekjames.uiproject.Restaurants_Main.class);
+                        startActivity(intent4);
+                        return true;
+                }
+                return true;
+            }
+        });
+        ////////////////////////////////////////
+
     }
 }
--- app/src/main/java/com/example/derekjames/uiproject/Restaurants_Main.java
+++ app/src/main/java/com/example/derekjames/uiproject/Restaurants_Main.java
@@ -1,8 +1,12 @@
 package com.example.derekjames.uiproject;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -19,7 +23,7 @@ public class Restaurants_Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants__main);
 
-
+    ////////////creates and formats the listView
         String[] restaurantsArray = new String[] {
                 "Eagles Nest",
                 "Common Grounds",
@@ -40,6 +44,46 @@ public class Restaurants_Main extends AppCompatActivity {
             }
         };
         restaurantListView.setAdapter(arrayAdapter);
+////////////////////////////////////////////////////////////////
+
+
+        /////Switch between tabs
+
+        BottomNavigationView tabBar = (BottomNavigationView) findViewById(R.id.tabBar);
+        tabBar.setSelectedItemId(R.id.restaurantsicon);
+        tabBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.chimesicon : ///////Go to chimes page
+                       Intent intent1 = new Intent(Restaurants_Main.this,
+                            com.example.derekjames.uiproject.Chime_Main_Tab.class);
+                        startActivity(intent1);
+                        return true;
+                    case R.id.mapicon :
+                        Intent intent2 = new Intent(Restaurants_Main.this,
+                                com.example.derekjames.uiproject.Map.class);
+                        startActivity(intent2);
+                        return true;
+                    case R.id.buildinghoursicon :
+                        Intent intent3 = new Intent(Restaurants_Main.this,
+                                com.example.derekjames.uiproject.Building_Hours.class);
+                        startActivity(intent3);
+                        return true;
+                    case R.id.restaurantsicon :
+                        Intent intent4 = new Intent(Restaurants_Main.this,
+                                com.example.derekjames.uiproject.Restaurants_Main.class);
+                        startActivity(intent4);
+                        return true;
+                }
+                return true;
+            }
+        });
+        ////////////////////////////////////////
+
+
 
 
 

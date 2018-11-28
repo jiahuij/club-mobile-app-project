80c4ae6dc40507e4586e7afa6843310585f529da
derekj1 <derek.m.james@q.com>
2018-11-28 14:42:57 -0800

    Fixing Tab Bar

--- app/src/main/java/com/example/derekjames/uiproject/Building_Hours.java
+++ app/src/main/java/com/example/derekjames/uiproject/Building_Hours.java
@@ -2,9 +2,12 @@ package com.example.derekjames.uiproject;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +29,7 @@ public class Building_Hours extends AppCompatActivity {
 
         /////Switch between tabs
         BottomNavigationView tabBar = (BottomNavigationView) findViewById(R.id.tabBar);
+        disableShift(tabBar);
         tabBar.setSelectedItemId(R.id.buildinghoursicon);
         tabBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -89,8 +94,33 @@ public class Building_Hours extends AppCompatActivity {
         };
         restaurantListView.setAdapter(arrayAdapter);
         /////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
     }
+
+
+    private void  disableShift(BottomNavigationView view) {
+        BottomNavigationMenuView tabBar = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = tabBar.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(tabBar, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < tabBar.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) tabBar.getChildAt(i);
+                item.setShiftingMode(false);
+                item.setChecked(item.getItemData().isChecked());
+            }
+        }catch (NoSuchFieldException e)
+        {
+            Log.e("BNVHelper", "Unable to get shift mode field", e);
+        }catch (IllegalAccessException e)
+        {
+            Log.e("BNVHelper", "Unable to change value of shift mode", e);
+        }
+    }
+
+
+
+
+
+
 }

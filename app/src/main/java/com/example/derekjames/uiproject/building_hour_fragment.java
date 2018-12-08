7ca8ef66ceb2e6568a4995a666655c2a6171bf4d
jjh <jiahui.jiang@Biola.edu>
2018-12-08 13:02:08 -0800

    building hours

--- app/src/main/java/com/example/derekjames/uiproject/building_hour_fragment.java
+++ app/src/main/java/com/example/derekjames/uiproject/building_hour_fragment.java
@@ -1,5 +1,6 @@
 package com.example.derekjames.uiproject;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,11 +13,33 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class building_hour_fragment extends Fragment {
+
+
+     int buildingHoudCount = 0;
+
+     private  class building
+     {
+         String buildingName;
+         String buildingHours;
+         String currentStatus;
+
+         building(String buildingName, String buildingHours, String currentStatus)
+         {
+             this.buildingName = buildingName;
+             this.buildingHours = buildingHours;
+             this.currentStatus = currentStatus;
+         }
+     }building[] Building= new building[50];
+
 
     @Nullable
     @Override
@@ -25,14 +48,22 @@ public class building_hour_fragment extends Fragment {
 
         View view = inflater.inflate(R.layout.building_hours_fragment,container,false);
         ////////////////adds array into the listview
-        String[] restaurantsArray = new String[] {
-                "fd 1     12:00pm-8:00pm",
-                "Building 2     12:00pm-8:00pm",
-                "Building 3     12:00pm-8:00pm",
-                "Building 4     12:00pm-8:00pm",
-                "Building 5     12:00pm-8:00pm",
-                "Building 6     12:00pm-8:00pm",
-        };
+
+        try {
+            Void temp = new Connection().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+        String[] restaurantsArray = new String[buildingHoudCount];
+
+        for(int i = 0; i <buildingHoudCount; i++)
+        {
+            restaurantsArray[i] = Building[i].buildingHours;
+        }
 
         ListView listView = view.findViewById(R.id.buildingHour_listView);
 
@@ -45,6 +76,35 @@ public class building_hour_fragment extends Fragment {
         listView.setAdapter(listViewAdapter);
 
         return view;
+    }
+
+    public class Connection extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            org.jsoup.nodes.Document doc = null;
+            try {
+                doc = Jsoup.connect("https://www.biola.edu/chapel").get();
+
+
+                for (org.jsoup.nodes.Element row : doc.select(
+                        "ul.chapel-list.active li")) {
+
+                    if (row.select("li:nth-of-type(1).item-body.title").text().equals(" ")) {
+                        continue;
+                    } else {
+
+
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 
 

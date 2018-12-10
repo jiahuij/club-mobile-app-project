9f336574f3620e4d538483c4cc0953c32035d083
Derekj1 <DerekJGit>
2018-12-09 23:05:16 -0800

    Map Done, Chapel Done, Dining Done

--- app/src/main/java/com/example/derekjames/uiproject/Map.java
+++ app/src/main/java/com/example/derekjames/uiproject/Map.java
@@ -1,6 +1,7 @@
 package com.example.derekjames.uiproject;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
@@ -9,16 +10,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.ListView;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.concurrent.ExecutionException;
 
 public class Map extends AppCompatActivity {
+    private WebView Webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        /*Map*/
+        Webview = (WebView)findViewById(R.id.webview);
+        WebSettings webSettings = Webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+        Webview.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                view.loadUrl("javascript: var con = document.getElementById('content_head'); " + "con.parentNode.removeChild(con); ");
+            }
+        });
+        Webview.loadUrl("http://now.biola.edu/campus-maps/");
 
         /////Switch between tabs
         BottomNavigationView tabBar = (BottomNavigationView) findViewById(R.id.tabBar);
@@ -30,25 +54,25 @@ public class Map extends AppCompatActivity {
 
                 switch (item.getItemId()) {
 
-                    case R.id.chimesicon : ///////Go to chimes page
+                    case R.id.chimesicon: ///////Go to chimes page
                         Intent intent1 = new Intent(Map.this,
                                 com.example.derekjames.uiproject.Chime_Main_Tab.class);
                         intent1.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         startActivity(intent1);
                         return true;
-                    case R.id.mapicon :
+                    case R.id.mapicon:
                         Intent intent2 = new Intent(Map.this,
                                 com.example.derekjames.uiproject.Map.class);
                         intent2.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         startActivity(intent2);
                         return true;
-                    case R.id.buildinghoursicon :
+                    case R.id.buildinghoursicon:
                         Intent intent3 = new Intent(Map.this,
                                 com.example.derekjames.uiproject.Hours.class);
                         intent3.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                         startActivity(intent3);
                         return true;
-                    case R.id.restaurantsicon :
+                    case R.id.restaurantsicon:
                         Intent intent4 = new Intent(Map.this,
                                 com.example.derekjames.uiproject.Restaurants_Main.class);
                         intent4.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
@@ -58,37 +82,38 @@ public class Map extends AppCompatActivity {
                 return true;
             }
         });
-        ////////////////////////////////////////
     }
 
+    @Override
+    public void onBackPressed() {
+        if (Webview.canGoBack()) {
+            Webview.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-
-
-
-
-
-
-
-
-    private void  disableTabBarShift(BottomNavigationView view) {
+    private void disableTabBarShift(BottomNavigationView view)
+    {
         BottomNavigationMenuView tabBar = (BottomNavigationMenuView) view.getChildAt(0);
-        try {
+        try
+        {
             Field shiftingMode = tabBar.getClass().getDeclaredField("mShiftingMode");
             shiftingMode.setAccessible(true);
             shiftingMode.setBoolean(tabBar, false);
             shiftingMode.setAccessible(false);
-            for (int i = 0; i < tabBar.getChildCount(); i++) {
+            for (int i = 0; i < tabBar.getChildCount(); i++)
+            {
                 BottomNavigationItemView item = (BottomNavigationItemView) tabBar.getChildAt(i);
                 item.setShiftingMode(false);
                 item.setChecked(item.getItemData().isChecked());
             }
-        }catch (NoSuchFieldException e)
+        } catch (NoSuchFieldException e)
         {
             Log.e("BNVHelper", "Unable to get shift mode field", e);
-        }catch (IllegalAccessException e)
+        } catch (IllegalAccessException e)
         {
             Log.e("BNVHelper", "Unable to change value of shift mode", e);
         }
     }
-
 }
